3ff5edbe8b139bb23f2829c688b711b5afa4e523
Lekheshwar <hilekhesh20@gamil.com>
2021-03-24 23:25:09 +0530

    first commit

---
+++ imports/ui/Task.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+ 
+export const Task = ({ task, onCheckboxClick, onDeleteClick }) => {
+  return (
+  <li>
+    <input type="checkbox"
+    checked={!task.isChecked}
+    onClick={() => onCheckboxClick(task)}
+    readonly
+    />
+    <span>{task.text}</span>
+    <button onClick = { () => onDeleteClick(task)}>&times;</button>
+  </li>
+  );
+};
